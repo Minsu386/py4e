3e803e90f4f9c3d9eada82e1591fbc663f27858c
Minsu386 <38228979+Minsu386@users.noreply.github.com>
2024-04-02 05:37:54 -0400

    Initial Commit

---
+++ ch3IfElse.py
@@ -0,0 +1,17 @@
+x = 4
+if x > 2:
+    print('Bigger')
+else :
+    print('Smaller')
+
+print('All Done')
+
+# Multi-way if elseif
+if x < 2 :
+    print('small')
+elif x < 10 :
+    print('medium')
+else :
+    print('Large')
+print('All Done')
+
---
+++ input.py
@@ -0,0 +1,6 @@
+name = input('what is your name? \n')
+print('Hello', name)
+
+prompt = 'What...is the airspeed velocity of an unladen swallow? \n'
+speed = input(prompt)
+int(speed)
